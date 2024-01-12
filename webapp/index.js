4e9f22e871da14c821f8e4580e98eb41a47ee851
Couto, Eduardo L <coutoeduardol@johndeere.com>
2024-01-12 15:26:48 -0300

    Step 2

---
+++ webapp/index.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    [
+        'sap/m/Text',
+        'sap/m/Input'
+    ], 
+    
+    (Text, Input) => {
+        "use strict";
+
+        new Text({
+            text:"Eduardo Couto"
+        }).placeAt('ondeEuQuiser');
+}); 
